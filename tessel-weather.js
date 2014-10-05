0a087e173980095132233590ba24d2dee0352da9
Matt Gauger <matt.gauger@gmail.com>
2014-10-04 23:16:00 -0500

    After much strife, get the Tessel POSTing to Rails

--- tessel-weather.js
+++ tessel-weather.js
@@ -2,26 +2,22 @@ var tessel = require('tessel'),
     ambientLib = require('ambient-attx4'),
     climateLib = require('climate-si7020'),
     wifi = require('wifi-cc3000'),
-    http = require('http'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    needle = require('needle');
 
-var serverHost = '192.168.1.106',
-    serverPort = 3000,
-    secretToken = 'cookie', // change to production token
-    secondsBetweenReadings = 1000 * 5; // Once a minute
-
-var httpOptions = {
-  method: 'POST',
-  host: serverHost,
-  port: serverPort,
-  path: '/readings',
-};
-
+/* Hardware setup */
 var ambient = ambientLib.use(tessel.port['D']),
     climate = climateLib.use(tessel.port['A']);
 
 var led1 = tessel.led[0],
     led2 = tessel.led[1];
+
+/* Program constants */
+var serverHost = '192.168.1.106',
+    serverPort = 3000,
+    secretToken = 'cookie', // change to production token
+    secondsBetweenReadings = 1000 * 30, // Should be about once a minute in production
+    urlToPost = "http://" + serverHost + ":" + serverPort;
 
 var ledOn = function(led) {
   return led.write(1);
@@ -39,82 +35,81 @@ climate.on('error', function(err) {
   console.log('Error connecting module:', err);
 });
 
+var postCallback = function(error, response) {
+  if (!error && response && response.statusCode == 200) {
+    console.log('POST success');
+  } else { 
+    console.log('Error contacting server.', error);
+  }
+};
+
+var post = function(path, data) {
+  if (wifi.isConnected()) {
+    var payload = _.merge(data, {secret_token: secretToken});
+
+    needle.post(urlToPost + path, payload, postCallback);
+  } else {
+    console.log('Wifi not connected.');
+  }
+};
+
+var postTemperature = function(temp) {
+  if (!_.isUndefined(temp)) {
+    post('/temperatures', {temperature: {temperature_f: temp}});
+  }
+};
+
+var postHumidity = function(humidity) {
+  if (!_.isUndefined(humidity)) {
+    post('/humidities', {humidity: {humidity: humidity}});
+  }
+};
+
+var postSoundLevel = function(level) {
+  if (!_.isUndefined(level)) {
+    post('/sound_levels', {sound_level: {level: level}});
+  }
+};
+
+var postLightLevel = function(level) {
+  if (!_.isUndefined(level)) {
+    post('/light_levels', {light_level: {level: level}});
+  }
+};
+
 var loop = function() {
-  var temp, humidity, lightLevel, soundLevel;
+  ledOn(led1);
 
   climate.readTemperature('f', function (err, tempReading) {
-    temp = tempReading.toFixed(2);
+    var temp = tempReading.toFixed(2);
     console.log('Degrees:', temp + 'F');
+    postTemperature(temp);
   });
 
   climate.readHumidity(function (err, humid) {
-    humidity = humid.toFixed(4);
+    var humidity = humid.toFixed(4);
     console.log('Humidity:', humidity + '%RH');
-  });
-
-
-  ambient.getLightLevel(function(err, ldata) {
-    lightLevel =  ldata.toFixed(8);
-    console.log('Light Level:', lightLevel);
-  });
-
-
-  ambient.getSoundLevel(function(err, sdata) {
-    soundLevel = sdata.toFixed(8);
-    console.log('Sound Level:', soundLevel);
+    postHumidity(humidity);
   });
 
   ledOn(led2);
 
-  if (wifi.isConnected()) {
-    var timestamp = (new Date()).toJSON();
+  ambient.getLightLevel(function(err, ldata) {
+    var lightLevel =  ldata.toFixed(8);
+    console.log('Light Level:', lightLevel);
+    postLightLevel(lightLevel);
+  });
 
-    var payload = {
-      secret_token: secretToken,
-      reading: {
-        timestamp: timestamp,
-        temperature_f: temp,
-        humidity: humidity,
-        light_level: lightLevel,
-        sound_level: soundLevel
-      }
-    };
+  ambient.getSoundLevel(function(err, sdata) {
+    var soundLevel = sdata.toFixed(8);
+    console.log('Sound Level:', soundLevel);
+    postSoundLevel(soundLevel);
+  });
 
-    var respHandler = function(response) {
-      var responseStr = '';
-
-      response.setEncoding('utf8');
-
-      response.on('data', function(chunk) {
-        responseStr += chunk;
-      });
-
-      response.on('end', function() {
-        console.log('HTTP response:', responseStr);
-      });
-    };
-
-    var req = http.request(_.merge(httpOptions,
-                                   {headers: {
-                                     'Content-Type': 'application/json',
-                                     'Content-Length': payload.length
-                                   }}),
-                           respHandler);
-
-    req.on('error', function(e) {
-      console.log('HTTP request encountered error:', e);
-    });
-
-    console.log('POSTing at:', timestamp);
-    console.log('Payload:', payload);
-
-    req.write(JSON.stringify(payload));
-
-    req.end();
-  }
 
   ledOff(led1);
   ledOff(led2);
+
   setTimeout(loop, secondsBetweenReadings);
 };
 
